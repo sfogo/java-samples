6bc6956e452c59c9667d945226e29dd78e3ea13d
sfogo <o@gorostis.net>
2020-01-28 12:33:53 -0800

    Added SampleApp flux

--- reactor/src/test/java/com/vnet/samples/reactor/SampleAppTest.java
+++ reactor/src/test/java/com/vnet/samples/reactor/SampleAppTest.java
@@ -3,9 +3,16 @@ package com.vnet.samples.reactor;
 import org.testng.annotations.Test;
 
 public class SampleAppTest {
+    private final SampleApp app = new SampleApp();
+
     @Test
     public void testApp() {
-        final SampleApp app = new SampleApp();
         app.run("how", "long", "can", "we", "go", "on", "like", "this", "?");
     }
+
+    @Test
+    public void testDelayedValues() {
+        app.fluxOne(4, 31);
+    }
+
 }
