2e83d7ffd8efa2a49e55fa3a58bfa715c476e6eb
sfogo <o@gorostis.net>
2020-02-01 08:53:02 -0800

    Updated NE paths

--- problems/src/test/java/com/vnet/problems/NEPathsTest.java
+++ problems/src/test/java/com/vnet/problems/NEPathsTest.java
@@ -1,5 +1,6 @@
 package com.vnet.problems;
 
+import com.vnet.common.Sets;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,19 +12,32 @@ public class NEPathsTest {
         final NEPaths paths = new NEPaths();
         final List<List<NEPaths.Point>> list = paths.getPaths(5, 4);
         Assert.assertEquals(list.size(), 126);
+        // 10 = 1 + 5 + 4
+        log(list, 10);
+    }
 
+    @Test
+    public void testGetPaths22() {
+        final NEPaths paths = new NEPaths();
+        final List<List<NEPaths.Point>> list = paths.getPaths(2, 2);
+        Assert.assertEquals(list.size(), Sets.C(4,2));
+        // 5 = 1 + 2 + 2
+        log(list, 5);
+    }
+
+    private void log(final List<List<NEPaths.Point>> list, final int len) {
+        int i=1;
         for (final List<NEPaths.Point> path : list) {
-            // 10 = 1 + 5 + 4
-            Assert.assertEquals(path.size(), 10);
-//            String p = null;
-//            for (final NEPaths.Point point : path) {
-//                if (p == null) {
-//                    p = point.toString();
-//                } else {
-//                    p = p + " " + point.toString();
-//                }
-//            }
-//            System.out.println(p);
+            Assert.assertEquals(path.size(), len);
+            String p = null;
+            for (final NEPaths.Point point : path) {
+                if (p == null) {
+                    p = point.toString();
+                } else {
+                    p = p + " " + point.toString();
+                }
+            }
+            System.out.println(p + " : " + i++);
         }
     }
 }
